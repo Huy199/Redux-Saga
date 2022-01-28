24fac43796f26c1ed19e128a5ac83f1cf38c380e
huy <huyhq1102@gmail.com>
2022-01-29 00:43:48 +0700

    layout-login

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react';
 import cityApi from 'api/cityApi';
 import { Route, Switch } from 'react-router-dom';
-import { LoginPage } from 'features/auth/pages/LoginPage';
+import LoginPage from 'features/auth/pages/LoginPage';
 import { AdminLayout } from 'components/layout';
 import { NotFound } from 'components/common';
 import { PrivateRoute } from './components/common/PrivateRoute';
@@ -11,19 +11,17 @@ function App() {
     cityApi.getAll().then((response) => console.log('response: ', response));
   });
   return (
-    <div>
-      <Switch>
-        <Route path="/login">
-          <LoginPage />
-        </Route>
-        <PrivateRoute path="/admin">
-          <AdminLayout />
-        </PrivateRoute>
-        <Route>
-          <NotFound />
-        </Route>
-      </Switch>
-    </div>
+    <Switch>
+      <Route path="/login">
+        <LoginPage />
+      </Route>
+      <PrivateRoute path="/admin">
+        <AdminLayout />
+      </PrivateRoute>
+      <Route>
+        <NotFound />
+      </Route>
+    </Switch>
   );
 }
 
--- src/app/rootSaga.ts
+++ src/app/rootSaga.ts
@@ -1,11 +1,10 @@
 import counterSaga from 'features/counter/counterSaga';
-import {all} from 'redux-saga/effects';
-function* helloSaga(){
-    console.log("helloSaga: ");
+import { all } from 'redux-saga/effects';
+// function* helloSaga(){
+//     console.log("helloSaga: ");
 
-}
-export default function* rootSaga(){
-    console.log("rootSaga: ", rootSaga);
-    yield all([helloSaga(),counterSaga()])
-
+// }
+export default function* rootSaga() {
+  console.log('rootSaga: ', rootSaga);
+  yield all([counterSaga()]);
 }
